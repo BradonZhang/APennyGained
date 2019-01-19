2308d1c2fdccc81fa7e6209472e0ce0c0f5fa6a8
unkownfire <46833522+unkownfire@users.noreply.github.com>
2019-01-19 03:38:29 -0500

    Structure Changes
    Changed classes to fragments

--- app/src/main/java/com/swamphacks/uf/apennygained/MainActivity.java
+++ app/src/main/java/com/swamphacks/uf/apennygained/MainActivity.java
@@ -17,12 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         PieChartView pieChartView = findViewById(R.id.chart);
         List<SliceValue> pieData = new ArrayList<>();
-        pieData.add(new SliceValue(15, Color.BLUE).setLabel("Q1: $10"));
-        pieData.add(new SliceValue(25, Color.GRAY).setLabel("Q2: $4"));
-        pieData.add(new SliceValue(10, Color.RED).setLabel("Q3: $18"));
-        pieData.add(new SliceValue(60, Color.MAGENTA).setLabel("Q4: $28"));
         PieChartData pieChartData = new PieChartData(pieData);
         pieChartData.setHasLabels(true);
         pieChartView.setPieChartData(pieChartData);
