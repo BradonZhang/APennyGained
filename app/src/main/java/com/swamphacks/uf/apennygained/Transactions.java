caf11922ec1c0593a8bc1d6d7a6ef2d506071726
unkownfire <46833522+unkownfire@users.noreply.github.com>
2019-01-19 10:19:49 -0500

    Fixed another bug
    Transactions class changed

--- app/src/main/java/com/swamphacks/uf/apennygained/Transactions.java
+++ app/src/main/java/com/swamphacks/uf/apennygained/Transactions.java
@@ -1,15 +1,13 @@
 package com.swamphacks.uf.apennygained;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
+
+import com.swamphacks.uf.apennygained.R;
 
 public class Transactions extends AppCompatActivity {
 
@@ -28,6 +26,7 @@ public class Transactions extends AppCompatActivity {
                         switch(item.getItemId())
                         {
                             case R.id.action_chart:
+                                startActivity(new Intent(Transactions.this, MainActivity.class));
                                 break;
                             case R.id.action_categories:
                                 startActivity(new Intent(Transactions.this, Categories.class));
@@ -36,7 +35,7 @@ public class Transactions extends AppCompatActivity {
                                 startActivity(new Intent(Transactions.this, Settings.class));
                                 break;
                             case R.id.action_transactions:
-                                startActivity(new Intent(Transactions.this, Transactions.class));
+
                                 break;
                         }
                         return true;
