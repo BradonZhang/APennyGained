5801e3c80dff6d56e4ea5caa5afca605d995b938
Firestar493 <bradonzhang@gmail.com>
2019-01-19 23:31:35 -0500

    Dummy transactions can be received

--- app/src/main/java/com/swamphacks/uf/apennygained/Transaction.java
+++ app/src/main/java/com/swamphacks/uf/apennygained/Transaction.java
@@ -1,16 +1,29 @@
 package com.swamphacks.uf.apennygained;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+//import java.sql.Timestamp;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+
+import com.google.gson.JsonObject;
 
 public class Transaction {
-    private Timestamp timestamp;
-    String name;
-    double amount;
-    public Transaction(String name, double amount) {
-        timestamp = new Timestamp(System.currentTimeMillis());
-        this.name = name;
-        this.amount = amount;
+//    private Timestamp timestamp;
+    private String transactionId;
+    private double transactionAmount;
+    private String transactionDate;
+    private String transactionDescription;
+    public Transaction() {
+
+    }
+    public Transaction(JsonObject transaction) {
+//        timestamp = new Timestamp(System.currentTimeMillis());
+        this.transactionId = transaction.get("transactionId").getAsString();
+        this.transactionAmount = transaction.get("transactionAmount").getAsDouble();
+        this.transactionDate = transaction.get("transactionDate").getAsString();
+        this.transactionDescription = transaction.get("transactionDescription").getAsString();
+    }
+
+    public String toString() {
+        return transactionDescription;
     }
 }
--- app/src/main/java/com/swamphacks/uf/apennygained/Transactions.java
+++ app/src/main/java/com/swamphacks/uf/apennygained/Transactions.java
@@ -1,18 +1,37 @@
 package com.swamphacks.uf.apennygained;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.swamphacks.uf.apennygained.R;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class Transactions extends AppCompatActivity {
 
+    public static ArrayList<Transaction> getTransactions() {
+        // TODO: Actually import transactions from bank API
+        ArrayList<Transaction> transactions = new ArrayList<>();
+//        HashMap<String, Object> dummyTransaction = new HashMap<>();
+//        ObjectMapper mapper = new ObjectMapper();
+//        Map<String, Object> map = mapper.readValue(json, Map.class);
+
+        Gson gson = new Gson(); // Or use new GsonBuilder().create();
+//        Transaction target2 = gson.fromJson("{\"checkingAccountTransactions\": {    \"accountId\": \"da549a7cc86472ee05272c7bd0a4483f57174f2110e7ad961a267995031fda66c6d5475de467a65739750107b621e5a01be7cc0dc085a825fa384795904293f6\",    \"checkNumber\": 1007,    \"currencyCode\": \"USD\",    \"debitCreditMemo\": \"DEBIT\",    \"displayAccountNumber\": \"XXXXX1035\",    \"transactionAmount\": 12.22,    \"transactionDate\": \"2016-04-15\",    \"transactionDescription\": \"AUTOMATED PHONE + TRANSFER FROM April 15 10:35 5058\",    \"transactionDescriptionExtension\": \"TELEPHONE Reference# 545226\",    \"transactionId\": \"0507777777777000001519171200000\",    \"transactionStatus\": \"POSTED\",    \"transactionType\": \"PAYMENT\"}}", Transaction.class); // deserializes json into target2
+//        Transaction dummyTransaction = gson.fromJson("{    \"transactionAmount\": 12.22,    \"transactionDate\": \"2016-04-15\",    \"transactionDescription\": \"AUTOMATED PHONE + TRANSFER FROM April 15 10:35 5058\",    \"transactionId\": \"0507777777777000001519171200000\"}\",    \"checkNumber\": 1007,    \"currencyCode\": \"USD\",    \"debitCreditMemo\": \"DEBIT\",    \"displayAccountNumber\": \"XXXXX1035\",    \"transactionAmount\": 12.22,    \"transactionDate\": \"2016-04-15\",    \"transactionDescription\": \"AUTOMATED PHONE + TRANSFER FROM April 15 10:35 5058\",    \"transactionDescriptionExtension\": \"TELEPHONE Reference# 545226\",    \"transactionId\": \"0507777777777000001519171200000\",    \"transactionStatus\": \"POSTED\",    \"transactionType\": \"PAYMENT\"}", Transaction.class); // deserializes json into target2
+        Transaction dummyTransaction = gson.fromJson("    {    \"transactionAmount\": 12.22,    \"transactionDate\": \"2016-04-15\",    \"transactionDescription\": \"AUTOMATED PHONE + TRANSFER FROM April 15 10:35 5058\",    \"transactionId\": \"0507777777777000001519171200000\"}", Transaction.class);
+        transactions.add(dummyTransaction);
+        return transactions;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("Hello world!");
+        System.out.println(getTransactions());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactions);
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
